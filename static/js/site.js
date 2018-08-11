48a2777e4f90df443b8e15c18ad38986e261007c
root <root@atlantishq.de>
2018-08-11 20:11:07 +0200

    fixed js

--- static/js/site.js
+++ static/js/site.js
@@ -1,46 +1,22 @@
-var SemanticUIColors = "red orange yellow olive green teal blue violet purple pink brown".split(" ");
-
 var onload = function() {
-/*
-  $(".ui.accordion").accordion();
-  setSemanticUIColor();
-  $("#tag-category-pop").click(function() {
-    $(".hand.point.icon").toggleClass("active");
-  });
-
-
-  $('.dream-flip-toggle').click(function () {
-    var target = $('.pusher')
-    if (bodyBgSwitchIndex === 0) {
-      bodyBgSwitchIndex = 1
-      target.toggleClass('flip-it')
-      setTimeout( function () {
-        $('.front').css('display','none')
-        $('.back').css('display','block')
-      }, 300) 
+    var colors = "red orange yellow olive green teal blue violet purple pink brown".split(" ");
+    var tags   = document.getElementsByClassName('ui label red')
+    
+    /* elements are removed once their class name changes */
+    var i = 0
+    while (tags.length) {
+        if(tags[0].contains("blue")){
+            break;
+        }
+        console.log("wtf: "+tags[0].classList)
+        tags[0].classList[2] = "blue"
+        //colors[Math.floor(Math.random() * colors.length)];
+        console.log("tfw: "+"'"+tags[0].classList[2]+"'")
+        i++;
+        if(i > 20){
+            break;
+        }
     }
-      else {
-        bodyBgSwitchIndex = 0      
-        target.toggleClass('flip-it')
-        setTimeout( function (){
-          $('.front').css('display','block')
-        $('.back').css('display','none')
-      }, 300)
-    }
-  })  
-*/
 };
-function randomInt(a, b) {
-  a = Math.ceil(a);
-  b = Math.floor(b);
-  return random = Math.floor(Math.random() * (b - a)) + a;
-}
 
-function setSemanticUIColor() {
-  for (var a = [$(".dream-tags"), $(".sidebar-dream-tags")], b = 0; b < a.length; b++) {
-    a[b].children().map(function() {
-      $(this).addClass(SemanticUIColors[randomInt(0, SemanticUIColors.length)]);
-    });
-  }
-}
 document.addEventListener('DOMContentLoaded',onload);
