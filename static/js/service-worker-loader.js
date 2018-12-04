bc33eca6e14a30d04c1a69fdb54074a5064f5bb1
Sheppy <sheppy@atlantishq.de>
2018-12-04 11:06:54 +0100

    fix service worker scope

--- static/js/service-worker-loader.js
+++ static/js/service-worker-loader.js
@@ -1,4 +1,4 @@
 /* this could be done inline */
 if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/js/service-worker.js');
+    navigator.serviceWorker.register('/service-worker.js');
 }
