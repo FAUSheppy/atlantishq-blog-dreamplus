4a77e4b42faad86c7e24bcc1ec9081b8a2907b18
Sheppy <sheppy@atlantishq.de>
2018-12-04 11:25:00 +0100

    fix service worker typos

--- static/atlantishq-offline-worker.js
+++ static/atlantishq-offline-worker.js
@@ -1,30 +1,24 @@
-// proper initialization
-//if( 'function' === typeof importScripts) {
-    //importScripts('/js/polyfill.min.js');
-    self.addEventListener('install', function(e) {
-        console.log("INSTALL")
-        e.waitUntil(
-            caches.open('airhorner').then(function(cache) {
-              return cache.addAll([
-                    '/',
-                    '/index.html',
-                    '/css/main-style.css',
-                    '/js/lazyload.js',
-                  ]);
-            })
-        );
-    });
-    self.addEventListener('activate', event => {
-        event.waitUntil(self.clients.claim());
-    });
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open('athq').then(function(cache) {
+          return cache.addAll([
+                '/',
+                '/index.html',
+                '/css/main-style.css',
+                '/js/lazyload.js',
+                '/manifest.json',
+              ]);
+        })
+    );
+});
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
 
-    self.addEventListener('fetch', function(event) {
-      console.log(event.request.url);
-      console.log("FETCH");
-      event.respondWith(
-          caches.match(event.request).then(function(response) {
-                  return response || fetch(event.request);
-          })
-      );
-    });
-//}
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+      caches.match(event.request).then(function(response) {
+              return response || fetch(event.request);
+      })
+  );
+});
--- static/js/service-worker-loader.js
+++ static/js/service-worker-loader.js
@@ -1,4 +1,4 @@
 /* this could be done inline */
 if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js');
+    navigator.serviceWorker.register('/atlantishq-offline-worker.js');
 }
