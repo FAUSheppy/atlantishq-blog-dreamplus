e8d37b831f03a0be8fbb321dec23329f7070c054
Sheppy <sheppy@atlantishq.de>
2019-06-02 18:11:49 +0200

    burst cache

--- static/js/service-worker-loader.js
+++ static/js/service-worker-loader.js
@@ -1,4 +1,4 @@
 /* this could be done inline */
 if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/atlantishq-offline-worker.js');
+    navigator.serviceWorker.register('/atlantishq-offline-worker-1.js');
 }
